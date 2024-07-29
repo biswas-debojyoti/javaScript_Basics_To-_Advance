06552ac88bde935b582addd1ab5b872011e81370
DEBOJYOTI BISWAS <105899504+biswas-debojyoti@users.noreply.github.com>
2024-07-29 09:42:27 +0000

    04_dataTypeConvertion

---
+++ javaScript/04_dataTypeConvertion.js
@@ -0,0 +1,33 @@
+let roll = undefined;
+
+let changeRoll = Number(roll);
+
+// console.log(changeRoll)
+// console.log(typeof changeRoll)
+
+
+// for number convertion 
+
+
+// "23" => 23, number
+// "23qwe" => NAN, number
+// "asdad" => NAN, number
+// true => 1, number
+// false => 0, number
+// null => NAN, number
+// undefined => NAN, number
+// NAN => NAN, number
+
+let city = false;
+let changeCity = String(city)
+console.log(changeCity);
+console.log(typeof changeCity);
+
+
+let login = changeCity;
+let changeLogin = Number(changeCity)
+console.log(changeLogin);
+console.log(typeof changeLogin);
+
+
+
