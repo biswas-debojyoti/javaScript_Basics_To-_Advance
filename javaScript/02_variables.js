fa9d9a783058f00f6ff535bc1bfc80feb10d28b0
DEBOJYOTI BISWAS <105899504+biswas-debojyoti@users.noreply.github.com>
2024-07-29 08:33:28 +0000

    02_variables

---
+++ javaScript/02_variables.js
@@ -0,0 +1,16 @@
+// let , var , const
+
+const name = "deboyjoti";
+// we can't change const value 
+
+
+var roll = 23;
+let city = "kolkata";
+state = "westbengal";
+
+
+var roll = 25;
+city = "pune";
+state = "goa"
+
+console.table([roll, city, state])
